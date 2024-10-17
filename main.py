3b38dbed19c46e6be808337dcb0f431c92adb262
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-17 11:48:08 +0530

    program script ready

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ import pandas as pd
 df = pd.read_csv('hotels.csv')
 
 class Hotel:
+    def __init__(self, id) -> None:
+        pass
+
     def book(self):
         pass
-
-    def view_hotels(self):
+    
+    def available(self):
         pass
-
 
 class ReservationTicket:
+    def __init__(self, customer_name , hotel_object):
+        pass
 
     def generate(self):
-        pass
+        content = f"Name of the customer hotel: "
+        return content
 
 
 print(df)
+id = input('enter id of hotel: ')
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("enter name: ")
+    reservation = ReservationTicket(name, hotel)
+    print(reservation.generate())
+else:
+    print("hotel not available")
