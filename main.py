d2f337c7a39c1f6d88bf8e2c581e716582d875b8
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-18 11:46:01 +0530

    added child class SecureCreditCard and added authenticate method

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('hotels.csv', dtype={'id':str})
 df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+df_card_security = pd.read_csv('card_security.csv', dtype=str)
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -49,17 +50,29 @@ class CreditCard:
             return True
         else:
             return False
+        
+class SecureCreditCard(CreditCard):
+    def autheticate(self, given_password):
+        password = df_card_security.loc[df_card_security['number'] == self.number, 'password'].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
 print(df)
 hotel_ID = input('enter id of hotel: ')
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = CreditCard(number='1234567890123456')
+    credit_card = SecureCreditCard(number='1234567890123456')
     if credit_card.validate(expiration='12/26', holder='JOHN SMITH', cvc='123'):
-        hotel.book()
-        name = input("enter name: ")
-        reservation = ReservationTicket(customer_name=name, hotel_object= hotel)
-        print(reservation.generate())
+        if credit_card.autheticate(given_password='mypass'):
+            hotel.book()
+            name = input("enter name: ")
+            reservation = ReservationTicket(customer_name=name, hotel_object= hotel)
+            print(reservation.generate())
+        else:
+            print("Credit Card authentication failed")
     else:
         print("There was a problem with your payment!")
 else:
