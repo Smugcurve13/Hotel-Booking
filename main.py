32ee9c90c3ed00c49ccc9dbc645d1f3f549753cb
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-18 11:00:17 +0530

    added CreditCard class with validate method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('hotels.csv', dtype={'id':str})
-
+df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -38,14 +38,29 @@ Hotel Name: {self.hotel.name}
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+
+    def validate(self,expiration, holder, cvc):
+        card_data = {'number': self.number, 'expiration': expiration, 'holder': holder, 'cvc': cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
 print(df)
 hotel_ID = input('enter id of hotel: ')
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("enter name: ")
-    reservation = ReservationTicket(customer_name=name, hotel_object= hotel)
-    print(reservation.generate())
+    credit_card = CreditCard(number='1234567890123456')
+    if credit_card.validate(expiration='12/26', holder='JOHN SMITH', cvc='123'):
+        hotel.book()
+        name = input("enter name: ")
+        reservation = ReservationTicket(customer_name=name, hotel_object= hotel)
+        print(reservation.generate())
+    else:
+        print("There was a problem with your payment!")
 else:
     print("hotel not available")
